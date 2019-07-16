d331341ef3a1909bf2ea815a62b175b5dbac4144
Lab71_Maq17 <evertonwalixyt@gmail.com>
2019-07-16 16:23:02 -0300

    Versão 3

--- js/script.js
+++ js/script.js
@@ -1,28 +1,57 @@
-function consulta(city_name){
+$( document ).ready(function() {//função para tecla "Enter"
+    $( "#search-btn" ).click(Consulta);
+    $( "#search" ).on("keypress", function(e) {
+        if (e.keyCode == 13) {
+            $("#search-btn").click();
+        }
+    });
+    Consulta(); 
+});
 
-    var cidade = document.getElementById("nome_cidade").value
+function Consulta() {
 
+    var cidade = $( "#search" ).val();
+    $( "#search" ).val("");
+
+    if (cidade == "") {
+        cidade = "Quebrangulo"; //cidade padrão
+    }
+    
     $.getJSON({
         url: "https://api.hgbrasil.com/weather",
         data: {
-            // be65aab9
-            key: '5dd88861',
-            format: 'json-cors',
-            city_name: cidade
+            key: "be65aab9",// be65aab9
+            format: "json-cors",
+            city_name: cidade,
+            locale: "pt", //site sempre em pt-br
         },
-        success: function( result ){
-            data = result.results;
-            city_name = data.city_name;
 
-            $("#city").html("Clima em " + city_name);
-            $("#temp").html ("Temperatura atual: " + data.temp + " °C");
-            $("#description").html ("Descrição: " + data.description);
-            $("#wind").html ("Velocidade do vento: " + data.wind_speedy);
-            $("#sunrise").html ("Nascer do sol: " + data.sunrise);
-            $("#sunset").html ("Por do sol: " +data.sunset );
+        type: "GET",
+        dataType: "json",
 
-            var url_img = "http://assets.api.hgbrasil.com/weather/images/" + data.img_id +".png";
-            $("#img").attr("src", url_img);
+    }).done(function( r1 ) {
+         
+        if (r1.by == "default") { // if para cidade não encontrada "by = default"
+            $( "#city" ).html("CIDADE NÃO ENCONTRADA!");
+            $("#temp").html ("");
+            $("#description").html ("");
+            $("#wind").html ("");
+            $("#sunrise").html ("");
+            $("#sunset").html ("");
+            $( "#img" ).attr("src", "");
+        } 
+        
+        else {
+            d = r1.results;
+            $("#city").html("Clima em " + d.city_name);// se a cidade for encontrada
+            $("#temp").html ("Temperatura atual: " + d.temp + " °C");
+            $("#description").html ("Descrição: " + d.description);
+            $("#wind").html ("Velocidade do vento: " + d.wind_speedy);
+            $("#sunrise").html ("Nascer do sol: " + d.sunrise);
+            $("#sunset").html ("Por do sol: " + d.sunset );
+
+            var imagem = "http://assets.api.hgbrasil.com/weather/images/" + d.img_id +".png";
+            $("#img").attr("src", imagem);
         }
-    })
+    });
 }
